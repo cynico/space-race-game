e98dbe63d6ad38df2885848596f29fe57a5a944e
cynic0 <ysi.rabie@gmail.com>
2023-04-16 17:23:22 +0200

    req 7 done

--- source/common/material/material.cpp
+++ source/common/material/material.cpp
@@ -2,12 +2,40 @@
 
 #include "../asset-loader.hpp"
 #include "deserialize-utils.hpp"
+#include "glad/gl.h"
+#include "shader/shader.hpp"
+
+#include <iostream>
 
 namespace our {
 
     // This function should setup the pipeline state and set the shader to be used
     void Material::setup() const {
+        
         //TODO: (Req 7) Write this function
+        pipelineState.setup();
+        
+        /*
+            if (dynamic_cast<const TexturedMaterial*>(this)) {
+                shader->attach("assets/shaders/textured.vert", GL_VERTEX_SHADER);
+                shader->attach("assets/shaders/textured.frag", GL_FRAGMENT_SHADER);
+            } else if (dynamic_cast<const TintedMaterial*>(this)) {
+                std::cout << "tinted" << std::endl;
+                if (!shader->attach("assets/shaders/tinted.vert", GL_VERTEX_SHADER)) {
+                    std::cerr << "error attaching vertex shader" << std::endl;
+                } else {
+                    std::cout << "correct" << std::endl;
+                }
+                if (!shader->attach("assets/shaders/tinted.frag", GL_FRAGMENT_SHADER)) {
+                    std::cerr << "error attaching fragment shader" << std::endl;
+                } else {
+                    std::cout << "correct" << std::endl;
+                }
+                if (!shader->link()) {
+                    std::cout << "why" << std::endl;
+                }
+            }
+        */
     }
 
     // This function read the material data from a json object
@@ -24,7 +52,12 @@ namespace our {
     // This function should call the setup of its parent and
     // set the "tint" uniform to the value in the member variable tint 
     void TintedMaterial::setup() const {
+        
         //TODO: (Req 7) Write this function
+        Material::setup();
+        shader->use();
+        shader->set("tint", tint);
+
     }
 
     // This function read the material data from a json object
@@ -38,7 +71,25 @@ namespace our {
     // set the "alphaThreshold" uniform to the value in the member variable alphaThreshold
     // Then it should bind the texture and sampler to a texture unit and send the unit number to the uniform variable "tex" 
     void TexturedMaterial::setup() const {
+        
         //TODO: (Req 7) Write this function
+
+        Material::setup();
+        
+        // Using the shader program and setting the alphaThreshold uniform.
+        shader->use();
+        shader->set("alphaThreshold", alphaThreshold);
+
+        // Binding the texture and the sampler to texture unit 0.
+        glActiveTexture(GL_TEXTURE0);
+        glBindTexture(GL_TEXTURE_2D, texture->getOpenGLName());
+        sampler->bind(0);
+        
+        // Sending the value 0 as the value of the uniform tex.
+        shader->set("tex", 0);
+        shader->set("tint", tint);
+
+
     }
 
     // This function read the material data from a json object
--- source/states/material-test-state.hpp
+++ source/states/material-test-state.hpp
@@ -1,5 +1,7 @@
 #pragma once
 
+#include "material/material.hpp"
+#include "mesh/mesh.hpp"
 #include <asset-loader.hpp>
 #include <ecs/transform.hpp>
 #include <application.hpp>
